501343f6371bb9f00a1cda9ca1585e567f3768fb
VictorSampaio0906 <victorcs.dev@gmail.com>
2025-04-22 09:40:54 -0300

    variavel idade e cidade

--- style/main.js
+++ style/main.js
@@ -4,3 +4,14 @@ const nome = params.get("nome");
 if (nome) {
   document.getElementById("nomeUsuario").textContent = nome;
 }
+
+const cidade = params.get("cidade");
+const idade = params.get("idade");
+
+if (cidade) {
+  document.getElementById("cidade").textContent = cidade;
+}
+
+if (idade) {
+  document.getElementById("idade").textContent = idade;
+}
